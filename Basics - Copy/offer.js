8f0ee66f7b8155a723261def578f7bfade621015
Vansh Sukhija <12112021@nitkkr.ac.in>
2022-03-22 19:46:17 +0530

    added Dev's files

---
+++ Basics - Copy/offer.js
@@ -0,0 +1,31 @@
+var i=document.getElementById("offer1")
+
+function fun(){
+    alert("Use code FREEDOM to get cashback upto Rs.500 on domestic flight bookings")
+    console.log("click");
+}
+i.addEventListener("click",fun);
+
+var i=document.getElementById("offer2")
+
+function fun(){
+    alert("Use code FREEDOM to get cashback upto 50% off on hotel bookings")
+    console.log("click");
+}
+i.addEventListener("click",fun);
+
+var i=document.getElementById("offer3")
+
+function fun(){
+    alert("Use code POOL to get cashback upto Rs.100 on carpool")
+    console.log("click");
+}
+i.addEventListener("click",fun);
+
+var i=document.getElementById("offer4")
+
+function fun(){
+    alert("Use code FOODY to get 50% cashback upto Rs.100 on first order")
+    console.log("click");
+}
+i.addEventListener("click",fun);
